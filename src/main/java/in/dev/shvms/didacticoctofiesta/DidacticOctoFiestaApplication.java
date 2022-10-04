2051d375dccd986687bc62079e2e712433288606
shivam <dev.shvms@gmail.com>
2022-10-04 20:31:40 +0530

    Initial commit

---
+++ src/main/java/in/dev/shvms/didacticoctofiesta/DidacticOctoFiestaApplication.java
@@ -0,0 +1,13 @@
+package in.dev.shvms.didacticoctofiesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DidacticOctoFiestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DidacticOctoFiestaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/dev/shvms/didacticoctofiesta/DidacticOctoFiestaApplicationTests.java
@@ -0,0 +1,13 @@
+package in.dev.shvms.didacticoctofiesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DidacticOctoFiestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
